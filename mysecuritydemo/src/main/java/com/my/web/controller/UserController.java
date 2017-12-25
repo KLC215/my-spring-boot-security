bb88dd5a9b059a9f22329d31525fc33198226ca3
KLC215 <kwanlamc05@gmail.com>
2017-12-25 22:21:45 +0800

    Update get a user and create user function, handle date, and validate form data

--- mysecuritydemo/src/main/java/com/my/web/controller/UserController.java
+++ mysecuritydemo/src/main/java/com/my/web/controller/UserController.java
@@ -1,16 +1,16 @@
 package com.my.web.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.my.dto.User;
 import com.my.dto.UserQueryParameters;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +19,8 @@ import java.util.List;
 public class UserController {
 
 	@GetMapping()
-	public List<User> query(UserQueryParameters query, @PageableDefault(size = 10, page = 0, sort = "username,asc") Pageable pageable) {
+	@JsonView(User.UserGeneralJsonView.class)
+	public List<User> index(UserQueryParameters query, @PageableDefault(size = 10, page = 0, sort = "username,asc") Pageable pageable) {
 		System.out.println(ReflectionToStringBuilder.toString(query, ToStringStyle.MULTI_LINE_STYLE));
 		System.out.println(pageable.getPageSize());
 		System.out.println(pageable.getPageNumber());
@@ -32,6 +33,30 @@ public class UserController {
 		users.add(new User());
 
 		return users;
+	}
+
+	@GetMapping("/{id:\\d+}")
+	@JsonView(User.UserDetailJsonView.class)
+	public User show(@PathVariable Integer id) {
+		User user = new User();
+		user.setUsername("tom");
+		return user;
+	}
+
+	@PostMapping()
+	public User store(@Valid @RequestBody User user, BindingResult errors) {
+
+		if (errors.hasErrors()) {
+			errors.getAllErrors()
+				  .forEach(error -> System.out.println(error.getDefaultMessage()));
+		}
+
+		System.out.println(user.getUsername());
+		System.out.println(user.getPassword());
+		System.out.println(user.getDob());
+
+		user.setId(1);
+		return user;
 	}
 
 }
